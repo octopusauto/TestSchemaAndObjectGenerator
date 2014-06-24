7546b33d6a8979d762d4218064cee2c952e39c7d
Octopus <reutinus@yahoo.com>
2014-06-24 16:00:18 +0530

    Test Schema and object generator tool
    Test Schema and object generator tool

---
+++ TestSchemaGenerator/src/main/java/RunSchemaGen.java
@@ -0,0 +1,22 @@
+import com.thaiopensource.relaxng.translate.Driver;
+
+
+public class RunSchemaGen {
+	
+       
+	
+	   public static void main(String []args) throws Exception
+	    {
+		   //Driver driver = new Driver();
+		   Driver.main(args);
+		   /* Process ps=Runtime.getRuntime().exec("java -jar trang.jar data.xml data.xsd");
+	        ps.waitFor();
+	        java.io.InputStream is=ps.getInputStream();
+	        byte b[]=new byte[is.available()];
+	        is.read(b,0,b.length);
+	        System.out.println(new String(b));*/
+		   System.out.println("Heloooooooooooooo");
+	    }
+	}
+
+
